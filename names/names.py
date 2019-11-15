c2748edeee430d490cad605de31a2b0768502d1a
OmarSalah95 <Omars.e.995@gmail.com>
2019-11-15 06:23:47 -0800

    Worked out that I want a Binary Search tree for my solution to the names problem. Now building it

--- names/names.py
+++ names/names.py
@@ -11,10 +11,27 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
+# Is this solution O(n2)?? This seems like an insane way to go about this, but Others seem similarly nuts.
+# First thing to come to mind being I would like to cut this down to n log n at the least if not log n. If I want to do so
+# I am going to need both of those long lists to be sorted, and issue in and of itself with its own complexity in the best of cases
+# being Log n,
+# I am going to need a more robust algorithmicly architected data structure to make this possible. I think a graph would work best
+# But a Binary Searhc tree will work great for this for now
+"""for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
-            duplicates.append(name_1)
+            duplicates.append(name_1)"""
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+bt = BinarySearchTree(names_1)
+for name in names_2:     # This is O(n) as we iterate and repeat this step
+    if name in bt.value:
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
